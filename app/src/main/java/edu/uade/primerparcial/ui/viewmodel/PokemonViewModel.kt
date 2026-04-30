8c83b766ef30dcd06cd75230bc2e0d337eab2d0c
botrera <braian@houlak.com>
2026-04-30 19:29:14 -0300

    Refactorizacion del codigo

---
+++ app/src/main/java/edu/uade/primerparcial/ui/viewmodel/PokemonViewModel.kt
@@ -0,0 +1,23 @@
+package edu.uade.primerparcial.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import edu.uade.primerparcial.data.Pokemon
+import edu.uade.primerparcial.repository.PokemonRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class PokemonViewModel(
+    private val repository: PokemonRepository = PokemonRepository()
+) : ViewModel() {
+    private val _pokemons = MutableStateFlow<List<Pokemon>>(emptyList())
+    val pokemons: StateFlow<List<Pokemon>> = _pokemons.asStateFlow()
+
+    init {
+        loadPokemons()
+    }
+
+    private fun loadPokemons() {
+        _pokemons.value = repository.getPokemons()
+    }
+}
